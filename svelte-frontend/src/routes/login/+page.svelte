ed3a0e603351868c8cc9d0ec4e53c866e3b83800
diegovhdev <diego.valencia.dev@gmail.com>
2026-02-25 23:10:28 -0500

    feat: se agrego la estructura del frontend

---
+++ svelte-frontend/src/routes/login/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+
+  let userRequest = $state({
+    username: "",
+    password: ""
+  })
+
+</script>
+
+<form class="flex-col">
+  <div>
+    <label for="username">username: </label>
+    <input type="text" name="username" id="username" bind:value={userRequest.username}>
+  </div>
+</form>
